e9065998a279b3805086f1701ac02a3c899e0122
jfbarahonag <jfbarahonag@unal.edu.co>
2024-09-28 19:46:55 -0500

    adding the functionality to get the workload by team and email

--- app/controllers/teams_controller.py
+++ app/controllers/teams_controller.py
@@ -6,11 +6,24 @@ from app.services.teams_service import TeamsService
 
 router = APIRouter(prefix="/teams")
 
-# Endpoint para obtener un ticket
+# Endpoint para obtener los miembros de un team
 @router.get("/{team_name}/members")
 def get_ticket(team_name: str):
     try:
         members = TeamsService.get_team_members(team_name)
+        return {
+            "status": "success",
+            "data": members,
+        }
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+# Endpoint para obtener los tickets asignados de un miembro de un team
+@router.get("/{team_name}/members/{member_email}/assigned")
+def get_tickets_by_email(team_name: str, member_email: str):
+    try:
+        ## TODO: Change this
+        members = TeamsService.get_tickets_by_member(team_name, member_email)
         return {
             "status": "success",
             "data": members,
--- app/services/teams_service.py
+++ app/services/teams_service.py
@@ -32,3 +32,30 @@ class TeamsService:
             }
         else:
             raise ValueError(f"Error al obtener los miembros del equipo: {response.status_code} - {response.content.decode()}")
+        
+    @staticmethod
+    def get_tickets_by_member(team_name: str, user_email: str):
+        """
+        Obtiene los tickets asignados a un usuario por su correo electrónico.
+        """
+        # Filtrar tickets por estado y asignado a usuario
+        url = f"{AZURE_ORG_URL}/{PROJECT_NAME}/{team_name}/_apis/wit/wiql?api-version=7.1"
+        query = {
+            "query": f"SELECT [System.Id], [System.Title], [System.State], [System.AssignedTo] "
+                     f"FROM WorkItems "
+                     f"WHERE [System.AssignedTo] = '{user_email}' "
+                     f"AND [System.State] = 'En evaluacion'"
+                     f"AND [System.AreaPath] = '{PROJECT_NAME}\\{team_name}'"
+        }
+
+        response = requests.post(url, headers=create_headers(content_type="application/json"), json=query)
+
+        if response.status_code == 200:
+            work_items = response.json()["workItems"]
+            tickets = [{"id": item["id"]} for item in work_items]
+            return {
+                "count": len(tickets),
+                "tickets": tickets    
+            }
+        else:
+            raise ValueError(f"Error al obtener los tickets: {response.status_code} - {response.content.decode()}")
